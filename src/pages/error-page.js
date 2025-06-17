2f4ce23687db716c0851881b89cacf419f324bcb
Hojin Yang <dahlia.jazzes0@icloud.com>
2025-06-17 21:01:09 +0900

    add missing base url

--- src/pages/error-page.js
+++ src/pages/error-page.js
@@ -9,7 +9,7 @@ export function NotFoundErrorPage() {
     h(
       "div",
       { class: "flex flex-col md:flex-row gap-12.5 items-center" },
-      h("img", { class: "w-69 aspect-[69/59]", src: "/images/icon-404.svg", alt: "" }),
+      h("img", { class: "w-69 aspect-[69/59]", src: `${import.meta.env.BASE_URL}images/icon-404.svg`, alt: "" }),
       h("section", { class: "flex flex-col max-w-103.5" }, [
         h("h2", { class: "font-bold text-4xl mb-5" }, "페이지를 찾을 수 없습니다."),
         h(
