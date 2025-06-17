e3c6a23bea91d5b161a0efc8f9b1b3fc0ab38235
Hojin Yang <dahlia.jazzes0@icloud.com>
2025-06-17 20:55:06 +0900

    export button style

--- src/shared/ui/button.js
+++ src/shared/ui/button.js
@@ -1,12 +1,12 @@
 import { tv } from "tailwind-variants";
 import { h } from "../element-helper/element-helper";
 
-const style = tv({
+export const buttonStyle = tv({
   base: "flex w-full items-center justify-center text-center hover:opacity-95 active:opacity-90",
   variants: {
     size: {
       sm: "h-10 rounded-sm",
-      md: "h-15 rounded-sm",
+      md: "h-15 rounded-sm font-bold",
     },
     variant: {
       primary: "bg-brand text-white",
@@ -25,5 +25,5 @@ const style = tv({
 });
 
 export function Button({ children, size, variant, disabled, class: className, ...rest }) {
-  return h("button", { class: style({ size, variant, disabled, className }), ...rest }, ...children);
+  return h("button", { class: buttonStyle({ size, variant, disabled, className }), ...rest }, ...children);
 }
