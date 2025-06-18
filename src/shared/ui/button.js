c17660323bedd87e676f1c26b4f98fed591b66cd
Hojin Yang <dahlia.jazzes0@icloud.com>
2025-06-18 10:12:44 +0900

    correct button style

--- src/shared/ui/button.js
+++ src/shared/ui/button.js
@@ -2,19 +2,22 @@ import { tv } from "tailwind-variants";
 import { h } from "../element-helper/element-helper";
 
 export const buttonStyle = tv({
-  base: "flex w-full items-center justify-center text-center hover:opacity-95 active:opacity-90",
+  base: "flex w-full items-center justify-center gap-x-2 rounded-sm text-center",
   variants: {
     size: {
-      sm: "h-10 rounded-sm",
-      md: "h-15 rounded-sm font-bold",
+      sm: "h-10 font-medium",
+      input: "h-13.5 font-medium",
+      md: "h-15 font-bold",
+      lg: "h-17 font-bold",
     },
     variant: {
-      primary: "bg-brand text-white",
-      secondary: "bg-gray-3 text-white",
-      outline: "border-gray-4 text-gray-3 border",
+      primary: "bg-brand text-white hover:opacity-95 active:opacity-90",
+      secondary: "bg-gray-3 text-white hover:opacity-95 active:opacity-90",
+      outline:
+        "border-gray-4 text-gray-3 hover:border-gray-3 active:border-gray-3 border hover:text-black active:text-black",
     },
     disabled: {
-      true: "bg-gray-4 text-white",
+      true: "bg-gray-4 cursor-not-allowed text-white",
     },
   },
   defaultVariants: {
