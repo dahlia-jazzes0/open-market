c3b1e39c2f410b89b5db1aef05e97e370562a75d
Hojin Yang <dahlia.jazzes0@icloud.com>
2025-06-19 23:48:36 +0900

    add form things

---
+++ src/features/form/create-form.js
@@ -0,0 +1,129 @@
+import { createSignal } from "@/shared/element-helper/element-helper";
+import { SchemaError } from "@/shared/schema/schema-error";
+
+/**
+ * @template T
+ * @typedef {() => T} ReadSignal
+ */
+
+/**
+ * @template T
+ * @typedef {(newValue: T | ((prevValue: T) => T)) => void} WriteSignal
+ */
+
+/**
+ * @template Value
+ * @typedef {{
+ *   name: string,
+ *   value: ReadSignal<Value>,
+ *   setValue: WriteSignal<Value>,
+ *   onblur: (e: Event & { currentTarget: HTMLFormElement }) => void,
+ *   errors: ReadSignal<unknown[]>,
+ *   setErrors: WriteSignal<unknown[]>,
+ *   invalidate: (value: unknown, options?: { resolve?: (value: Value) => void, reject?: (error: unknown) => void }) => void,
+ *   isValid: () => boolean,
+ * }} Field
+ */
+
+/**
+ * @template {Record<string, { parse: () => any; }>} Fields
+ * @typedef {{
+ *   fields: { [Key in keyof Fields]: Field<ReturnType<Fields[Key]["parse"]>> },
+ *   onsubmit: (e: Event & { currentTarget: HTMLFormElement }) => Promise<void>
+ * }} Form
+ */
+
+/**
+ * @template {Record<string, { parse: () => any; }>} Fields
+ * @param {{
+ *   fields: Fields,
+ *   onsubmit: (
+ *     data: { [Key in keyof Fields]: ReturnType<Fields[Key]["parse"]> },
+ *     options: { form: Form<Fields>, formElement: HTMLFormElement }
+ *   ) => Promise<void> | void,
+ *   onerror: (
+ *     data: unknown,
+ *     options: { form: Form<Fields>, formElement: HTMLFormElement }
+ *   ) => Promise<void> | void,
+ * }} options
+ * @returns {Form<Fields>}
+ */
+export function createForm({ fields, onsubmit, onerror }) {
+  const form = {
+    fields: {},
+    onsubmit: async (e) => {
+      e.preventDefault();
+      const formElement = e.currentTarget;
+      const data = {};
+      let hasError = false;
+      let hasFocused = false;
+      for (const [name, field] of Object.entries(form.fields)) {
+        const element = formElement.elements[name];
+        if (element == null) throw new Error("Element not found");
+        field.invalidate(element.value, {
+          resolve: (value) => {
+            data[name] = value;
+          },
+          reject: () => {
+            if (!hasFocused) element.focus();
+            hasFocused = true;
+            hasError = true;
+          },
+        });
+      }
+      if (hasError) return;
+      try {
+        await onsubmit(data, { form, formElement });
+      } catch (err) {
+        onerror?.(err, { form, formElement });
+      }
+    },
+  };
+  for (const [name, fieldConfig] of Object.entries(fields)) {
+    const [value, setValue] = createSignal();
+    const [errors, setErrors] = createSignal([]);
+    const onblur = (e) => {
+      invalidate(e.currentTarget.value, {
+        reject: () => {
+          setValue(e.currentTarget.value);
+        },
+      });
+    };
+    const isValid = () => {
+      try {
+        fieldConfig.parse(value());
+        return true;
+      } catch (error) {
+        return false;
+      }
+    };
+
+    form.fields[name] = {
+      name,
+      value,
+      setValue,
+      onblur,
+      errors,
+      setErrors,
+      invalidate,
+      isValid,
+    };
+
+    function invalidate(value, options) {
+      try {
+        const parsedValue = fieldConfig.parse(value);
+        setValue(parsedValue);
+        setErrors([]);
+        options?.resolve?.(parsedValue);
+      } catch (error) {
+        if (error instanceof SchemaError) {
+          setErrors([error.message]);
+          options?.reject?.(error);
+          return;
+        }
+        throw error;
+      }
+    }
+  }
+  return form;
+}
---
+++ src/features/form/ui.js
@@ -0,0 +1,71 @@
+import { For, h } from "@/shared/element-helper/element-helper";
+import { tv } from "tailwind-variants";
+
+export function FormField({ form, name, render }) {
+  const id = crypto.randomUUID();
+  const field = form.fields[name];
+  if (field == null) throw new Error(`Field ${name} not found in form`);
+  return h("div", { class: "flex flex-col gap-y-2.5" }, () =>
+    render({
+      id,
+      name,
+      ...field,
+    }),
+  );
+}
+
+const formInputStyle = tv({
+  base: "border-gray-4 focus:border-brand invalid:border-red h-13.5 w-full rounded-sm border bg-white p-4 focus:outline-none",
+  variants: {
+    state: {
+      invalid: "border-red",
+    },
+  },
+});
+export function FormInput({ field, class: className, children, ...rest }) {
+  return h("input", {
+    class: () => formInputStyle({ className, state: field.errors().length > 0 ? "invalid" : "" }),
+    id: field.id,
+    name: field.name,
+    value: field.value,
+    onblur: field.onblur,
+    ...rest,
+  });
+}
+
+const formLabelStyle = tv({
+  base: "text-gray-3",
+});
+export function FormLabel({ field, class: className, children, ...rest }) {
+  return h(
+    "label",
+    {
+      class: formLabelStyle({ className }),
+      for: field.id,
+      ...rest,
+    },
+    children,
+  );
+}
+
+export function FormErrors({ field, class: className }) {
+  return h(For, {
+    each: field.errors,
+    render: (error) => h(FormErrorView, { className }, error),
+    resolveKey: (error) => error,
+  });
+}
+
+const formErrorViewStyle = tv({
+  base: "text-red",
+});
+function FormErrorView({ class: className, children, ...rest }) {
+  return h(
+    "p",
+    {
+      class: formErrorViewStyle({ className }),
+      ...rest,
+    },
+    ...children,
+  );
+}
