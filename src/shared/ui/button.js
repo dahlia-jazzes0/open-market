4a9f0ea9421c300d2c3046126eb9f5990bf23239
Hojin Yang <dahlia.jazzes0@icloud.com>
2025-06-19 21:38:34 +0900

    fix button disabled state

--- src/shared/ui/button.js
+++ src/shared/ui/button.js
@@ -2,7 +2,7 @@ import { tv } from "tailwind-variants";
 import { h } from "../element-helper/element-helper";
 
 export const buttonStyle = tv({
-  base: "flex w-full items-center justify-center gap-x-2 rounded-sm text-center",
+  base: "flex w-full items-center justify-center gap-x-2 rounded-sm text-center disabled:cursor-not-allowed",
   variants: {
     size: {
       sm: "h-10 font-medium",
@@ -11,22 +11,18 @@ export const buttonStyle = tv({
       lg: "h-17 font-bold",
     },
     variant: {
-      primary: "bg-brand text-white hover:opacity-95 active:opacity-90",
-      secondary: "bg-gray-3 text-white hover:opacity-95 active:opacity-90",
+      primary: "bg-brand disabled:bg-gray-4 text-white hover:opacity-95 active:opacity-90 disabled:text-white",
+      secondary: "bg-gray-3 disabled:bg-gray-4 text-white hover:opacity-95 active:opacity-90 disabled:text-white",
       outline:
         "border-gray-4 text-gray-3 hover:border-gray-3 active:border-gray-3 border hover:text-black active:text-black",
-    },
-    disabled: {
-      true: "bg-gray-4 cursor-not-allowed text-white",
     },
   },
   defaultVariants: {
     size: "md",
     variant: "primary",
-    disabled: false,
   },
 });
 
-export function Button({ children, size, variant, disabled, class: className, ...rest }) {
-  return h("button", { class: buttonStyle({ size, variant, disabled, className }), ...rest }, ...children);
+export function Button({ children, size, variant, class: className, ...rest }) {
+  return h("button", { class: buttonStyle({ size, variant, className }), ...rest }, ...children);
 }
