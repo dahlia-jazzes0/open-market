86bb07f8dd93e196246133f6c87527bc617daeb9
Hojin Yang <dahlia.jazzes0@icloud.com>
2025-06-20 12:41:09 +0900

    separate auth tab style

--- src/pages/(auth)/login-page.js
+++ src/pages/(auth)/login-page.js
@@ -6,8 +6,8 @@ import { createSignal, For, fr, h, Show } from "@/shared/element-helper/element-
 import { Link, router } from "@/shared/router/router";
 import { Button } from "@/shared/ui/button";
 import { Input } from "@/shared/ui/input";
-import { tv } from "tailwind-variants";
 import { AuthLayout } from "./auth-layout";
+import { authTabStyle } from "./auth-tab-style";
 
 export function LoginPage() {
   const form = createForm({
@@ -90,64 +90,6 @@ export function LoginPage() {
     ]),
   );
 }
-
-const authTabStyle = tv({
-  slots: {
-    list: "grid grid-cols-2",
-    trigger:
-      "bg-gray-6 border-gray-4 relative h-20 rounded-t-md border-x border-t pt-5 text-center text-lg font-medium",
-    triggerOrnament: "border-gray-4 absolute -right-0.25 bottom-0 -left-0.25 h-2.5 bg-white",
-    panel: "border-gray-4 w-full rounded-b-md border-x border-b bg-white p-8.75 pt-6.25",
-  },
-  variants: {
-    active: {
-      true: "",
-    },
-    position: {
-      first: {
-        trigger: "",
-      },
-      last: {
-        trigger: "",
-      },
-    },
-  },
-  compoundVariants: [
-    {
-      active: false,
-      position: "first",
-      class: {
-        triggerOrnament: "rounded-tl-md border-t border-l",
-      },
-    },
-    {
-      active: false,
-      position: "last",
-      class: {
-        triggerOrnament: "rounded-tr-md border-t border-r",
-      },
-    },
-    {
-      active: true,
-      position: "first",
-      class: {
-        trigger: "bg-white",
-        triggerOrnament: "border-l bg-white",
-      },
-    },
-    {
-      active: true,
-      position: "last",
-      class: {
-        trigger: "bg-white",
-        triggerOrnament: "border-r bg-white",
-      },
-    },
-  ],
-  defaultVariants: {
-    active: false,
-  },
-});
 
 function LoginTabView(props) {
   const style = authTabStyle();
